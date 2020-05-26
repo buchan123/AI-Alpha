dba5e7484cefbf0df8c200e1b6efeae50af4a1a3
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2020-05-26 17:16:58 +0530

    added extension.py for custom zipline bundle support

---
+++ scripts/extension.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+from zipline.data.bundles import register
+from zipline.data.bundles.csvdir import csvdir_equities
+
+
+start_session = pd.Timestamp('2015-5-15', tz='utc')
+end_session = pd.Timestamp('2020-5-15', tz='utc')
+
+
+register(
+    'eod-yahoofinance',
+    csvdir_equities(
+        ['daily'],
+        '/home/siddhant/Desktop/Major-Project/data',
+    ),
+    calendar_name='XBOM', 
+    start_session=start_session,
+    end_session=end_session
+)
